f1c773050d5347eed33c8331afb59b08fe0a1164
zied <zied@localhost.localdomain>
2020-06-16 10:23:05 +0100

    first commit

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('Register')
+                    ->assertSee('Register')
+                    ->value('#name','user1')
+                    ->value('#email','user@gmail.com')
+                    ->value('#password','123456789')
+                    ->value('#password-confirm','123456789')
+                    ->click('button[type="submit"]')
+                    ->assertPathIs('/home');
+        });
+    }
+}
